719c94547251d11374f972fbbdf6416501f78c82
Raphael Sauer <raphael.sauer@recalm.com>
2024-04-30 15:25:34 +0200

    added accelerometer to timimg_test, added funcionality to use teleplot-compatible-message

--- MDSolarCleaner_arduino/src/timing_test.cpp
+++ MDSolarCleaner_arduino/src/timing_test.cpp
@@ -1,40 +1,96 @@
 #include <Arduino.h>
 #include <SPI.h>
+#include <MPU6050.h>
 #include "MDSolarCleaner_functions.h"
 #include "HC-SR04.h"
 #include "pinDefines.h"
 
-bool serialTimingTest    (float distance_up, float distance_down, float distance_right,
-                         float distance_left /*, float temperature, float surface_temp,
-                         float humidity, int air_dust_density, float brush1_speed,
-                         float brush2_speed, float flow_velocity, float acceleration*/) 
+enum SerialMSGMode {teleplot = 0, raspberrypi = 1};
+
+SerialMSGMode serialMSGmode = raspberrypi;
+
+bool serialTimingTest_PI  (float distance_up, 
+                          float distance_down, 
+                          float distance_right,
+                          float distance_left, 
+                          // float temperature, 
+                          // float surface_temp,
+                          // float humidity, 
+                          // int air_dust_density, 
+                          // float brush1_speed,
+                          // float brush2_speed, 
+                          // float flow_velocity, 
+                          float acceleration) 
 {
-  String sensorString = String(distance_up) + ";" + String(distance_down) + ";" +
-                        String(distance_right) + ";" + String(distance_left);/* + ";" +
-                        String(temperature) + ";" + String(surface_temp) + ";" +
-                        String(humidity) + ";" + String(air_dust_density) + ";" +
-                        String(brush1_speed) + ";" + String(brush2_speed) + ";" +
-                        String(flow_velocity) + ";" + String(acceleration);*/
+  String sensorString = String(distance_up) + ";" + 
+                        String(distance_down) + ";" +
+                        String(distance_right) + ";" + 
+                        String(distance_left) + ";" +
+                        // String(temperature) + ";" + 
+                        // String(surface_temp) + ";" +
+                        // String(humidity) + ";" + 
+                        // String(air_dust_density) + ";" +
+                        // String(brush1_speed) + ";" + 
+                        // String(brush2_speed) + ";" +
+                        // String(flow_velocity) + ";" + 
+                        String(acceleration);
 
   Serial.println(sensorString);
   return 1;  // Wenn erfolgreich sende 1 sonst 0
 }
 
+bool serialTimingTest_TP (float distance_up, 
+                          float distance_down, 
+                          float distance_right,
+                          float distance_left, 
+                          // float temperature, 
+                          // float surface_temp,
+                          // float humidity, 
+                          // int air_dust_density, 
+                          // float brush1_speed,
+                          // float brush2_speed, 
+                          // float flow_velocity, 
+                          float acceleration) 
+{
+  Serial.print(">Dist_up:");
+  Serial.println(distance_up);
+  Serial.print(">Dist_down:");
+  Serial.println(distance_down);
+  Serial.print(">Dist_right:");
+  Serial.println(distance_right);
+  Serial.print(">Dist_left:");
+  Serial.println(distance_left);
+  Serial.print(">Accel_z:");
+  Serial.println(acceleration);
+  return 1;  // Wenn erfolgreich sende 1 sonst 0
+}
+
+// Ultraschallsensoren
 HCSR04 USdistRight  (US_RIGHT_TRIG, US_RIGHT_ECHO);
 HCSR04 USdistLeft   (US_LEFT_TRIG, US_LEFT_ECHO);
 HCSR04 USdistUp     (US_UP_TRIG, US_UP_ECHO);
 HCSR04 USdistDown   (US_DOWN_TRIG, US_DOWN_ECHO);
+
+// IMU
+MPU6050 mpu;
 
 // Sensorvariablen
 float distance_right = 0.0;         // [cm]
 float distance_left = 0.0;          // [cm]
 float distance_up = 0.0;            // [cm]
 float distance_down = 0.0;          // [cm]
+float acceleration = 0.0;
 
 void setup()
 {
     Serial.begin(9600);
+    
+    // Pin für Zeitmessung der Schleife
     pinMode(MEASURE_TIME_PIN, OUTPUT);
+    
+    // Accelerometer
+    Wire.begin();
+    mpu.initialize();
 }
 
 void loop()
@@ -44,7 +100,24 @@ void loop()
     distance_up    = USdistUp.readDistance();
     distance_down  = USdistDown.readDistance();
 
+    acceleration = read_acceleration(mpu);
+
+    
+    
+    
+    
+    // Set MEASURE_TIME_PIN for duration of serial-transfer
     digitalWrite(MEASURE_TIME_PIN, HIGH);
-    serialTimingTest(distance_up, distance_down, distance_right, distance_left);
+    switch (serialMSGmode)
+    {
+      case teleplot:
+        serialTimingTest_TP(distance_up, distance_down, distance_right, distance_left, acceleration);
+      break;
+      case raspberrypi:
+        serialTimingTest_PI(distance_up, distance_down, distance_right, distance_left, acceleration);
+      break;
+      default:
+      break;
+    }
     digitalWrite(MEASURE_TIME_PIN, LOW);
 }
