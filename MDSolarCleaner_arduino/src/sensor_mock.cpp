dab180b86d436c0bb23c782c3e9cfe221abf719c
Raphael Sauer <raphael.sauer@recalm.com>
2024-06-03 09:35:44 +0200

    Added new sensor_mock.cpp for new sensor layout

--- MDSolarCleaner_arduino/src/sensor_mock.cpp
+++ MDSolarCleaner_arduino/src/sensor_mock.cpp
@@ -2,42 +2,50 @@
 #include <SPI.h>
 #include "MDSolarCleaner_functions.h"
 
-float distance_up = 1.0;
-float distance_down = 1.0; 
-float distance_left = 1.0; 
-float distance_right = 1.0;
-float temperature = 21.0;
-float surface_temp = 21.0;
-float humidity = 55.0;
-float brush1_speed = 200;
-float brush2_speed = 200;
-float flow_velocity = 10;
-float acceleration = 0.5;
+#define SERIAL_BAUD 115200
+#define SENSOR_DATA_FIELDS 3
+#define SENSOR_MSG_FREQUENCY 100
+#define SENSOR_MSG_PERIOD_US 1000000/SENSOR_MSG_FREQUENCY
 
-int air_dust_density = 50;
+// Sensorvariable
+float sensorValues[SENSOR_DATA_FIELDS] = {0., 0., 0.};
+
+// Loop timing variables
+unsigned long loopStartUs = 0;
+unsigned long loopDurationUs = 0;
+
+// sawtooth counter
+int counter = 0;
 
 void setup(){
-    Serial.begin(9600);
+    Serial.begin(115200);
     randomSeed(7524890);
 };
 
 void loop(){
-    send_data_to_raspy(distance_up, distance_down, distance_right, 
-                       distance_left, temperature, surface_temp,
-                       humidity, air_dust_density, brush1_speed, 
-                       brush2_speed, flow_velocity, acceleration
-                       );
-    distance_up         += random(-100, 101)/3000.0;
-    distance_down       += random(-100, 101)/3000.0; 
-    distance_right      += random(-100, 101)/3000.0; 
-    distance_left       += random(-100, 101)/3000.0; 
-    temperature         += random(-100, 101)/1000.0;
-    surface_temp        += random(-100, 101)/1000.0;
-    humidity            += random(-100, 101)/100.0;
-    air_dust_density    += random(-2, 3);
-    brush1_speed        += random(-100, 101)/10.0;
-    brush2_speed        += random(-100, 101)/10.0;
-    flow_velocity       += random(-100, 101)/100.0; 
-    acceleration        += random(-100, 101)/1000.0; 
-    delay(100);     // to be replaced by HW-Timer triggered Interrupt
+    
+    // Takt current Time
+    loopStartUs = micros();
+
+    // Generate "sawtooth" shaped sensor data
+    if(counter < 500)
+    {
+        sensorValues[0] += 1.;
+        sensorValues[1] += 1.;
+        sensorValues[2] += 1.;
+        counter++;
+    }
+    else
+    {
+        sensorValues[0] = 0.;
+        sensorValues[1] = 0.;
+        sensorValues[2] = 0.;
+        counter = 0;
+    }
+    // Write to serial
+    writeSensorvaluesToSerial(sensorValues, SENSOR_DATA_FIELDS);
+
+    // Loop timing
+    loopDurationUs = micros()-loopStartUs;
+    delayMicroseconds(SENSOR_MSG_PERIOD_US-loopDurationUs);
 };
