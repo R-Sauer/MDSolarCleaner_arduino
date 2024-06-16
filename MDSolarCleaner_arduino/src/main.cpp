228b02f72c1477f66b5e22b680ab410ab66b6e6d
Raphael Sauer <raphael.sauer@recalm.com>
2024-06-16 15:16:13 +0200

    Modified some names and comments for readablity

--- MDSolarCleaner_arduino/src/main.cpp
+++ MDSolarCleaner_arduino/src/main.cpp
@@ -9,7 +9,7 @@
 #define SENSOR_DATA_FIELDS 3
 #define SENSOR_MSG_FREQUENCY 100
 #define SENSOR_MSG_PERIOD_US 1000000/SENSOR_MSG_FREQUENCY
-#define BRUSH_GEAR_RATIO 2.0
+#define BRUSH_MOTOR_GEAR_RATIO 2.0
 #define BRUSH_MOTOR_MAGNETS_NUMBER 8
 
 // define ISRs for brush RPM
@@ -22,8 +22,8 @@ ISR(TIMER4_COMPA_vect);
 ACS712 acs(CURRENT_SENSOR_PIN, 5.0, 1023, 66);
 
 //TLE4935L Hall Effect Sensors
-TLE4935L brush1(RPM_BRUSH1_PIN, BRUSH_MOTOR_MAGNETS_NUMBER, BRUSH_GEAR_RATIO);
-TLE4935L brush2(RPM_BRUSH2_PIN, BRUSH_MOTOR_MAGNETS_NUMBER, BRUSH_GEAR_RATIO);
+TLE4935L brush1(RPM_BRUSH1_PIN, BRUSH_MOTOR_MAGNETS_NUMBER, BRUSH_MOTOR_GEAR_RATIO);
+TLE4935L brush2(RPM_BRUSH2_PIN, BRUSH_MOTOR_MAGNETS_NUMBER, BRUSH_MOTOR_GEAR_RATIO);
 
 // Sensorvariable
 float sensorValues[SENSOR_DATA_FIELDS];
@@ -52,21 +52,21 @@ void setup()
     TCCR4A = 0;// set entire TCCR1A register to 0
     TCCR4B = 0;// same for TCCR1B
     TCNT4  = 0;//initialize counter value to 0
-    // set compare match register for 1hz increments
-    OCR4A = 3905;// = (16*10^6) / (1*4096) - 1 (must be <65536)
+    // set compare match register for 4hz increments
+    OCR4A = 3905; // = (16*10^6) clock cycles / (1*4096) clock cycles - 1 (must be <65536)
     // turn on CTC mode
     TCCR4B |= (1 << WGM12);
     // Set CS12 and CS10 bits for 1024 prescaler
     TCCR4B |= (1 << CS12) | (1 << CS10);  
     // enable timer compare interrupt
     TIMSK4 |= (1 << OCIE4A);
-    // Reenable Interrupts
+    // Reenable interrupts
     sei();
 }
 
 void loop()
 {
-    // Takt current Time
+    // current time
     loopStartUs = micros();
 
     // Read momentary sensor values
